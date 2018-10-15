4940fafa97e8daee055a7c55eb0a3e5453a72d6f
pucheng <pucheng@xiaomalixing.com>
2018-10-15 19:26:30 +0800

    mi wakeup

---
+++ sample/src/main/java/com/aispeech/sample/AppKey.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright 2014 AISpeech
+ ******************************************************************************/
+package com.aispeech.sample;
+
+//TODO 修改完毕后去除
+public final class AppKey {
+
+    public static final String APPKEY = "1514535853458373";// 测试激活码用的appkey
+    public static final String SECRETKEY = "e08b337668eec500b43e005bc7972255";//添加您的SECRETKEY"
+    
+    
+}
---
+++ sample/src/main/java/com/aispeech/util/SampleConstants.java
@@ -0,0 +1,45 @@
+package com.aispeech.util;
+
+/**
+ * @author whyme
+ */
+public class SampleConstants {
+
+	/**
+	 *  VAD资源
+	 */
+	public final static String RES_VAD = "vad_aihome_v0.7.bin";
+
+	/**
+	 * AEC资源设置
+	 */
+	public final static String RES_AEC = "AEC_ch2-2-ch1_1ref_com_20171227_v0.8.5.bin";
+
+	/**
+	 * 识别相关资源
+	 */
+	public final static String RES_EBNFC = "ebnfc.aihome.0.3.0.bin";
+	public final static String RES_EBNFR = "ebnfr.aihome.0.3.0.bin";
+	public final static String RES_GRAMMAR = "grammar.xbnf";
+	public final static String local_asr_net_bin = "asr.net.bin";
+
+	/**
+	 *  唤醒资源文件
+	 */
+	public final static String RES_WAKEUP = "wakeup_aifar_comm_20180104.bin";
+
+
+	/**
+	 *  合成资源文件
+	 */
+	public final static String RES_TTS = "qianranc_common_param_ce_local.v2.022.bin";
+	public final static String RES_DICT = "aitts_sent_dict_v3.24.db";
+
+	/**
+	 *  服务器资源
+	 */
+	public final static String SERVER_PRO = "ws://s.api.aispeech.com:1028,ws://s.api.aispeech.com:80";
+	public final static String SERVER_GRAY = "ws://s-test.api.aispeech.com:10000";
+	public final static String RES_AIHOME = "aihome";
+	public final static String RES_AIROBOT = "airobot";
+}
